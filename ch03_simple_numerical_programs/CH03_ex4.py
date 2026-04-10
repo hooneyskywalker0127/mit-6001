1119b686c1be9487a818b61a877547b01b99968d
hooneyskywalker0127 <hooney0127@gmail.com>
2026-04-10 16:06:30 +0900

    Update CH03_ex4.py

--- ch03_simple_numerical_programs/CH03_ex4.py
+++ ch03_simple_numerical_programs/CH03_ex4.py
@@ -1,5 +1,47 @@
-# Use bisection search to find an approximation to the square root of x, where x = 123456789.
-# If no integer square root exists, print a message to that effect.
+# What would have to be changed to make the code in Figure 3.4 work for finding
+# an approximation to the cube root of both negative and positive numbers?
+# (Hint: think about changing low to ensure that the answer lies within the
+# region being searched.)
 #
-# 이분 검색을 사용하여 x = 123456789의 정수 제곱근을 구하는 프로그램을 작성하세요.
-# 정수 제곱근이 없다면 이를 알리는 메시지를 출력하세요.
+# 그림 3.4의 코드가 음수와 양수 모두의 세제곱근을 구할 수 있으려면 무엇을 바꿔야 할까요?
+# (힌트: 탐색 범위 안에 답이 포함되도록 low 값을 바꾸는 것을 생각해보세요.)
+#
+# [Figure 3.4]
+# x = 25
+# epsilon = 0.01
+# numGuesses = 0
+# low = 0.0
+# high = max(1.0, x)
+# ans = (high + low)/2.0
+# while abs(ans**2 - x) >= epsilon:
+#     numGuesses += 1
+#     if ans**2 < x:
+#         low = ans
+#     else:
+#         high = ans
+#     ans = (high + low)/2.0
+# print('numGuesses =', numGuesses)
+# print(ans, 'is close to square root of', x)
+
+x = -27
+epsilon = 0.01
+numGuesses = 0
+
+if x < 0:
+    low = min(-1.0, x)
+    high = 0.0
+else:
+    low = 0.0
+    high = max(1.0, x)
+
+ans = (high + low) / 2.0
+while abs(ans**3 - x) >= epsilon:
+    numGuesses += 1
+    if ans**3 < x:
+        low = ans
+    else:
+        high = ans
+    ans = (high + low) / 2.0
+
+print('numGuesses =', numGuesses)
+print(ans, 'is close to cube root of', x)
