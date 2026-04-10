70496930ee18b76bc5287b4fbd5dd65f087c6a65
hooneyskywalker0127 <hooney0127@gmail.com>
2026-04-10 16:20:06 +0900

    Update CH03_ex5.py

--- ch03_simple_numerical_programs/CH03_ex5.py
+++ ch03_simple_numerical_programs/CH03_ex5.py
@@ -1,18 +1,11 @@
-# Write a function convert_to_dollars that converts a float type number to dollars and cents,
-# and returns a string in dollars and cents.
+# What is the decimal equivalent of the binary number 10011?
 #
-# float 타입의 숫자를 달러(dollars)와 센트(cents)로 변환하여 문자열로 반환하는
-# convert_to_dollars 함수를 작성하세요.
-#
-# >>> convert_to_dollars(50.25)
-# '50 dollars and 25 cents'
-# >>> convert_to_dollars(12.01)
-# '12 dollars and 1 cents'
-# >>> convert_to_dollars(11.20)
-# '11 dollars and 20 cents'
-# >>> convert_to_dollars(1.12)
-# '1 dollar and 12 cents'
-# >>> convert_to_dollars(0.01)
-# '1 cent'
-# >>> convert_to_dollars(1.0)
-# '1 dollar'
+# 이진수 10011의 십진수 값은 무엇인가요?
+
+num = '10011'
+result = 0
+
+for bit in num:
+    result = result * 2 + int(bit)
+
+print(result)
